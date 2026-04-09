275ce2db4d0ac3da5ce089867ea84dbc08d48840
Harshini-2111 <23mh1a05o0@acoe.edu.in>
2026-04-09 12:28:27 +0530

    fix port and mongodb

--- backend/server.js
+++ backend/server.js
@@ -219,5 +219,5 @@ app.post('/reset-subject', authenticateToken, async (req, res) => {
     }
 });
 // Server Listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(` Server Running on Port ${PORT}`));
